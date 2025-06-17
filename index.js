1b7aaa2aebc3689f089f3f2753cdb870d4a4b1e2
asnicode <asneeboy2@gmail.com>
2025-06-17 02:07:43 -0400

    spain vs france

---
+++ index.js
@@ -0,0 +1,16 @@
+let goalGuest = document.getElementById("goal-count-guest")
+let goalHome = document.getElementById("goal-count-home")
+let count1 = 0
+function goalguest(){
+  count1 += 1
+  goalGuest.textContent = count1
+  
+  
+}
+let count2 = 0
+function goalhome(){
+  count2 += 1
+  goalHome.textContent = count2
+  
+  
+}
